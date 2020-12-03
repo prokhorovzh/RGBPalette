32bf25bcb580c9005ebd32d06adf9ab81ad86de3
Eugene <prokhorov.eu@icloud.com>
2020-12-04 03:48:23 +0400

    First

--- RGBPalette/ViewController.swift
+++ RGBPalette/ViewController.swift
@@ -9,11 +9,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redNumber: UILabel!
+    @IBOutlet weak var greenNumber: UILabel!
+    @IBOutlet weak var blueNumber: UILabel!
+    
+    @IBOutlet weak var paletteView: UIView!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redSlider.minimumValue = 0
+        redSlider.maximumValue = 255
+        redSlider.minimumTrackTintColor = .red
+        
+        greenSlider.minimumValue = 0
+        greenSlider.maximumValue = 255
+        greenSlider.minimumTrackTintColor = .green
+        
+        blueSlider.minimumValue = 0
+        blueSlider.maximumValue = 255
+        blueSlider.minimumTrackTintColor = .blue
+        
     }
 
-
+    @IBAction func redSliderAction(_ sender: Any) {
+        redNumber.text = String(Int(redSlider.value))
+    }
+    @IBAction func greenSliderAction(_ sender: Any) {
+        greenNumber.text = String(Int(greenSlider.value))
+    }
+    @IBAction func blueSliderAction(_ sender: Any) {
+        blueNumber.text = String(Int(blueSlider.value))
+    }
+    
 }
 
